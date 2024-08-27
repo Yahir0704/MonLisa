81482426b7316aaa3c68f953e6d925804206a58e
yahir <yahir@LAPTOP-H2INCEOF>
2024-08-27 17:59:55 -0600

    Agregar archivos de proyecto.

---
+++ MonLisa/MonLisa.cpp
@@ -0,0 +1,29 @@
+// MonLisa.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numeros[6] = { 10, 20, 30, 40, 50, 60 };
+    int n[] = { 3, 4, 5 };
+    char c[] = { 'L', 'U', 'I', 'S' };  
+    char s[] = "MONA LISA";  
+
+    cout << numeros[5] << "\n";  
+    cout << n[1] << "\n";  
+    cout << c[3] << "\n";  
+    cout << sizeof(s) << "\n";  
+    cout << sizeof(n) << "\n";  
+    cout << sizeof(numeros) << "\n";  
+
+    return 0;
+}
+
+
+
+
+
+
+
